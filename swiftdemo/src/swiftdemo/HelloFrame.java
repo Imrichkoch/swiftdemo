83f9a54ce8a555fbc4dd9d68e55b2a8df7166a48
Imrichkoch <imrichkoch@gmail.com>
2017-09-03 13:38:30 +0200

    spravne zarovnanie

--- swiftdemo/src/swiftdemo/HelloFrame.java
+++ swiftdemo/src/swiftdemo/HelloFrame.java
@@ -2,15 +2,10 @@ package swiftdemo;
 
 import javax.swing.JFrame;
 
-public class HelloFrame extends JFrame{
+public class HelloFrame extends JFrame {
 
-	
-	/**
-	 * 
-	 */
-	
 	private static final long serialVersionUID = -6658177492159480071L;
-	
+
 	public HelloFrame() {
 		add(new HelloWorldComponent());
 		pack();
--- swiftdemo/src/swiftdemo/HelloWorldComponent.java
+++ swiftdemo/src/swiftdemo/HelloWorldComponent.java
@@ -5,35 +5,23 @@ import java.awt.Graphics;
 
 import javax.swing.JComponent;
 
-public class HelloWorldComponent extends JComponent{
+public class HelloWorldComponent extends JComponent {
 
-	
-	
-	
-	
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 8979345813985698398L;
-	
-	
 
-	
-	private static final int DEFAULT_WIDTH = 300;
-	private static final int DEFAULT_HEIGHT = 200;
-	
-	private static final int MESSAGE_X = 150;
-	private static final int MESSAGE_Y = 100;
-	
-	
+	private static final int DEFAULT_WIDTH = 100;
+	private static final int DEFAULT_HEIGHT = 100;
+
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawString("Hello World", getWidth()/2, getHeight()/2);
+		String s = "Hello World";
+		int stringPixelLen = (int) g.getFontMetrics().getStringBounds(s, g).getWidth();
+		int startPosition = getWidth() / 2 - stringPixelLen / 2;
+		g.drawString(s, startPosition, getHeight() / 2);
 	}
-	
+
 	@Override
-	public Dimension getPreferredSize(){
+	public Dimension getPreferredSize() {
 		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 	}
 }
